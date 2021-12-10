0bc08a4145199b2555dab9d8756b94b07e0d3a59
Alex Zegarra <ZiggyManSaad@yahoo.com>
2021-12-10 19:28:14 +0000

    Fixed CoE to properly post to database

--- app/controllers/changes_controller.rb
+++ app/controllers/changes_controller.rb
@@ -1,5 +1,20 @@
 class ChangesController < ApplicationController
     def new
+        @change = Change.new
     end
     
+    def create
+    @change = Change.new(change_params)
+        if @change.save
+          flash[:success] = "Message Sent"
+          redirect_to new_change_path
+        else
+          flash[:danger] = @change.errors.full_messages.join(", ")
+          redirect_to new_change_path
+        end
+    end
+    private
+      def change_params
+        params.require(:change).permit(:name, :ADP)
+      end
 end
